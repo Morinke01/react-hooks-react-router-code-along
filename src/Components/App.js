03af595bcc0038ddd43cb26450d15c56833c1967
Morinke01 <ejulius250@gmail.com>
2023-08-10 15:52:58 +0300

    assignment completed

---
+++ src/Components/App.js
@@ -0,0 +1,18 @@
+ReactDOM.render(
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
+  
